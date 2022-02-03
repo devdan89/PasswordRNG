eb5b8a5b1c681a4b603b9f3dbb292bc4ef2d94b0
Dan <danilobomfim.si@gmail.com>
2022-02-03 19:36:27 -0300

    Changes

--- script.js
+++ script.js
@@ -1,30 +1,42 @@
 const newpw = document.querySelector(".content__pw-rng");
 console.log(newpw);
 
-
-
-
 function rng(qty) {
-  var letras = 'abcdefghijklmnopqrstuvwxyz';
-  var especiais = '!@#$%&*';
+  const letras = "abcdefghijklmnopqrstuvwxyz";
+  const especiais = "!@#$%&*";
   let pw = "";
   let sorteio = 0;
-  for (var i = 0; i <= qty - 1; i++) {
-    sorteio = Math.floor(Math.random() * 3) + 1;
-    console.log(sorteio);
-    switch (sorteio) {
-      case 1:
-        var index = letras.length;
-        pw += new String(letras[Math.floor(Math.random() * index)]);
-        break;
-      case 2:
+
+  //Checando a escolha dos caracteres da senha.
+  let checkboxLetras = document.getElementById("letras");
+  let checkboxNumeros = document.getElementById("numeros");
+  let checkboxEspeciais = document.getElementById("especiais");
+
+  //Gerador de senha.
+  if (
+    checkboxLetras.checked ||
+    checkboxNumeros.checked ||
+    checkboxEspeciais.checked
+  ) {
+    for (var i = 0; i <= qty - 1; ) {
+      sorteio = Math.floor(Math.random() * 3) + 1;
+
+      if (checkboxLetras.checked & (sorteio == 1)) {
+        pw += new String(letras[Math.floor(Math.random() * letras.length)]);
+        i++;
+      } else if (checkboxNumeros.checked & (sorteio == 2)) {
         pw += new String(Math.floor(Math.random() * 10));
-        break;
-      case 3:
-        var index = especiais.length;
-        pw += new String(especiais[Math.floor(Math.random() * index)]);
-        break;
+        i++;
+      } else if (checkboxEspeciais.checked & (sorteio == 3)) {
+        pw += new String(
+          especiais[Math.floor(Math.random() * especiais.length)]
+        );
+        i++;
+      }
     }
+    document.querySelector(".content__pw-rng").innerHTML = pw;
+  } else {
+    document.querySelector(".content__pw-rng").innerHTML =
+      "Você deve escolher pelo menos uma opção!";
   }
-  document.querySelector(".content__pw-rng").innerHTML = pw;
 }

79d481950ba3f3f33c58f158fb0538b52ba488cc
Dan <danilobomfim.si@gmail.com>
2022-03-02 18:49:46 -0300

    Fix

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const newpw = document.querySelector(".content__pw-rng");
-console.log(newpw);
 
 function rng(qty) {
   const letras = "abcdefghijklmnopqrstuvwxyz";
@@ -18,23 +16,30 @@ function rng(qty) {
     checkboxNumeros.checked ||
     checkboxEspeciais.checked
   ) {
-    for (var i = 0; i <= qty - 1; ) {
-      sorteio = Math.floor(Math.random() * 3) + 1;
+    if (qty >= 4 && qty <= 32) {
+      for (var i = 0; i <= qty - 1; ) {
+        sorteio = Math.floor(Math.random() * 3) + 1;
 
-      if (checkboxLetras.checked & (sorteio == 1)) {
-        pw += new String(letras[Math.floor(Math.random() * letras.length)]);
-        i++;
-      } else if (checkboxNumeros.checked & (sorteio == 2)) {
-        pw += new String(Math.floor(Math.random() * 10));
-        i++;
-      } else if (checkboxEspeciais.checked & (sorteio == 3)) {
-        pw += new String(
-          especiais[Math.floor(Math.random() * especiais.length)]
-        );
-        i++;
+        if (checkboxLetras.checked & (sorteio == 1)) {
+          pw += new String(letras[Math.floor(Math.random() * letras.length)]);
+          i++;
+        } else if (checkboxNumeros.checked & (sorteio == 2)) {
+          pw += new String(Math.floor(Math.random() * 10));
+          i++;
+        } else if (checkboxEspeciais.checked & (sorteio == 3)) {
+          pw += new String(
+            especiais[Math.floor(Math.random() * especiais.length)]
+          );
+          i++;
+        }
       }
+      document.querySelector(".content__pw-rng").innerHTML = pw;
+      
     }
-    document.querySelector(".content__pw-rng").innerHTML = pw;
+    else{
+      document.querySelector(".content__pw-rng").innerHTML =
+      "A senha precisa ter no mínimo 4 e no máximo 32 caracteres";
+    }
   } else {
     document.querySelector(".content__pw-rng").innerHTML =
       "Você deve escolher pelo menos uma opção!";
